e3ef3835f20fa81e9c3966bdab1931eb4abae416
julian <julian@pikhub.com>
2013-07-24 21:54:05 -0500

    setup updated

--- setup.py
+++ setup.py
@@ -13,10 +13,10 @@ setup(
     version=version,
     author=author,
     author_email='julian@pikhub.com',
-    url='http://github.com/julianceballos/conekta',
+    url='https://github.com/julianceballos/conekta',
     description='Easy Conekta python wrapper',
     long_description=open('./README.txt', 'r').read(),
-    download_url='http://github.com/julianceballos/conekta/tarball/master',
+    download_url='https://github.com/julianceballos/conekta/tarball/master',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
