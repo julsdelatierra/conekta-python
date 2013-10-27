5b9fed15b9b098d9cdcc6ef5da1ff53135d8c9e2
Jack London <leo2fischer@gmail.com>
2013-10-27 17:35:46 -0600

    restructured instances; added refunds, queries and logs

--- conekta/__init__.py
+++ conekta/__init__.py
@@ -4,6 +4,8 @@
 
 import base64
 import inspect
+import urllib
+import logging
 from httplib2 import Http
 
 try:
@@ -16,7 +18,7 @@ API_VERSION = '0.2.0'
 __version__ = '0.6'
 __author__ = 'Julian Ceballos'
 
-API_BASE = 'https://api.conekta.io/'
+API_BASE = 'http://localhost:3000/'
 
 HEADERS = {
     'Accept': 'application/vnd.conekta-v%s+json' % (API_VERSION),
@@ -25,67 +27,109 @@ HEADERS = {
 
 api_key = ''
 
-def parseJSON(obj):
-    data = {}
-    items = ()
-    if isinstance(obj, _CkObject):
-        items = obj.__dict__.iteritems()
-    else:
-        try:
-            items = json.loads(obj).iteritems()
-        except:
-            items = obj.iteritems()
-    for key, value in items:
-        try:
-            data[key] = parseJSON(value)
-        except AttributeError:
-            data[key] = value
-    return data
+class ConektaError(Exception):
+  def __init__(self, error_json):
+      super(ConektaError, self).__init__(error_json)
+      self.error_json = error_json
 
-class _CkObject(object):
-    def __init__(self, d):
-        self.__dict__['d'] = d
+class _Resource(object):
+    def __init__(self, attributes):
+        self.construct_from(attributes)
 
-    def __getattr__(self, key):
-        value = self.__dict__['d'][key]
-        if type(value) == type({}):
-            return _CkObject(value)
-        return value
+    def __getitem__(self, key):
+        return self.__dict__[key]
 
-    def parseJSON(self):
-        data = parseJSON(self)
-        return data['d']
+    def __setitem__(self, key, value):
+        self.__dict__[key] = value
 
-class _Endpoint(object):
-
-    def expand_path(self, path):
-        return API_BASE + path
-
-    def build_request(self, method, path, params):
+    @classmethod
+    def build_request(cls, method, path, params):
         HEADERS['Authorization'] = 'Basic %s' % (base64.b64encode(api_key + ':'))
-        absolute_url = self.expand_path(path)
+        absolute_url = API_BASE + path
         request = Http({}).request
-        headers, body = request(absolute_url, method, headers=HEADERS, body=json.dumps(params))
-        if headers['status'] == '200' or headers['status'] == '201':
-            return _CkObject(json.loads(body))
-        return _CkObject({'error': json.loads(body)})
+        if method == 'GET':
+            url = "%s?%s" % (absolute_url, urllib.urlencode(params))
+            headers, body = request(url, method, headers=HEADERS)
+        else:
+            headers, body = request(absolute_url, method, headers=HEADERS, body=json.dumps(params))
 
-    def load_url(self, path, method='get', params={}):
-        response = self.build_request(method, path, params)
+        if headers['status'] == '200' or headers['status'] == '201':
+            response_body = json.loads(body)
+            return response_body
+        raise ConektaError(json.loads(body))
+
+    @classmethod
+    def load_url(cls, path, method='GET', params={}):
+        response = cls.build_request(method, path, params)
         return response
 
-class _Charges(_Endpoint):
+    @classmethod
+    def class_name(cls):
+        return "%s" % urllib.quote_plus(cls.__name__.lower())
 
-    def create(self, params):
-        endpoint = 'charges.json'
-        return self.load_url(endpoint, method='post', params=params)
+    @classmethod
+    def class_url(cls):
+        return "%ss" % cls.class_name()
 
-class _Event(_Endpoint):
+    def instance_url(self):
+        return "%s/%s" % (self.class_url(), self.id)
 
-    def all(self):
-        endpoint = 'events.json'
-        return self.load_url(endpoint)
+    def construct_from(self, attributes):
+        if attributes['id']:
+            self.id = attributes['id']
+
+        existing_keys = self.__dict__.keys()
+        new_keys = attributes.keys()
+
+        old_keys = set(existing_keys) - set(new_keys)
+        for key in old_keys:
+            self.__dict__[key] = None
+
+        for key in new_keys:
+            self.__dict__[key] = attributes[key]
 
 
-Charge = _Charges()
-Event = _Event()
+    def refresh(self, url=None, method='POST', params={}):
+        if url is None:
+            url = self.instance_url()
+            method = 'GET'
+
+        response = self.load_url(url, method=method, params=params)
+
+        self.construct_from(response)
+
+        return self
+
+class _CreateableResource(_Resource):
+    @classmethod
+    def create(cls, params):
+        endpoint = cls.class_url()
+        return cls(cls.load_url(endpoint, method='POST', params=params))
+
+class _ListableResource(_Resource):
+    @classmethod
+    def retrieve(cls, _id):
+        endpoint = cls.class_url()
+        return cls(cls.load_url("%s/%s" % (endpoint, _id) ))
+
+    @classmethod
+    def all(cls, query={}, limit=10, offset=0, sort=[]):
+        endpoint = cls.class_url()
+        query['limit'] = limit
+        query['offset'] = offset
+        query['sort'] = sort
+        return [cls(attributes) for attributes in cls.load_url(endpoint, 'GET', query)]
+
+
+class Charge(_CreateableResource, _ListableResource):
+    def refund(self, amount=None):
+        if amount is None:
+            return self.refresh("%s/refund" % self.instance_url())
+        else:
+            return self.refresh("%s/refund" % self.instance_url(), 'POST', {'amount':amount})
+
+class Log(_ListableResource): pass
+
+class Event(_ListableResource): pass
+
+
--- tests/test_charges.py
+++ tests/test_charges.py
@@ -7,32 +7,50 @@ from . import BaseEndpointTestCase
 
 class OrdersEndpointTestCase(BaseEndpointTestCase):
 
-    def test_card_charge_done(self):
+    def test_card_charge_complete(self):
         self.client.api_key = '1tv5yJp3xnVZ7eK67m4h'
-        response = self.client.Charge.create(self.card_charge_object)
-        assert 'id' in response.parseJSON()
+        charge = self.client.Charge.create(self.card_charge_object)
+        assert charge.id
+        charge.refund(10000)
+        self.assertEqual(charge['status'], 'partially_refunded')
+        charge.refund()
+        self.assertEqual(charge['status'], 'refunded')
+        retrieved_charge = self.client.Charge.retrieve(charge.id)
+        assert charge.id
 
     def test_cash_charge_done(self):
         self.client.api_key = '1tv5yJp3xnVZ7eK67m4h'
-        response = self.client.Charge.create(self.cash_charge_object)
-        assert 'id' in response.parseJSON()
+        charge = self.client.Charge.create(self.cash_charge_object)
+        assert charge.id
 
     def test_bank_charge_done(self):
         self.client.api_key = '1tv5yJp3xnVZ7eK67m4h'
-        response = self.client.Charge.create(self.bank_charge_object)
-        assert 'id' in response.parseJSON()
+        charge = self.client.Charge.create(self.bank_charge_object)
+        assert charge.id
 
     def test_card_charge_authentication_fail(self):
         self.client.api_key = ''
-        response = self.client.Charge.create(self.card_charge_object)
-        assert 'authentication_error' == response.parseJSON()['error']['type']
+        self.assertRaises(self.client.ConektaError, self.client.Charge.create, self.card_charge_object)
 
     def test_cash_charge_authentication_fail(self):
         self.client.api_key = ''
-        response = self.client.Charge.create(self.cash_charge_object)
-        assert 'authentication_error' == response.parseJSON()['error']['type']
+        self.assertRaises(self.client.ConektaError, self.client.Charge.create, self.cash_charge_object)
 
     def test_bank_charge_authentication_fail(self):
         self.client.api_key = ''
-        response = self.client.Charge.create(self.bank_charge_object)
-        assert 'authentication_error' == response.parseJSON()['error']['type']
+        self.assertRaises(self.client.ConektaError, self.client.Charge.create, self.bank_charge_object)
+
+    def test_log_complete(self):
+        self.client.api_key = '1tv5yJp3xnVZ7eK67m4h'
+        logs = self.client.Log.all()
+        self.assertIsInstance(logs[0], self.client.Log)
+        retrieved_log = self.client.Log.retrieve(logs[0].id)
+        assert retrieved_log.id
+
+    def test_event_complete(self):
+        self.client.api_key = '1tv5yJp3xnVZ7eK67m4h'
+        events = self.client.Event.all()
+        self.assertIsInstance(events[0], self.client.Event)
+        retrieved_event = self.client.Event.retrieve(events[0].id)
+        assert retrieved_event.id
+
